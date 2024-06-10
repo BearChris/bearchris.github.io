6a75df6725e3e1cac0801f923f2a3657e3e0bb13
BearChris <bear007509@gmail.com>
2024-06-11 01:21:06 +0800

    adding contents

--- public/darkmode.js
+++ public/darkmode.js
@@ -4,13 +4,15 @@ $( document ).ready(function() {
     const root = document.querySelector(':root');
     $('.darkmode-btn').click(()=>{
       if(isdark == false){
-          root.style.setProperty('--iobg','#000');
-          root.style.setProperty('--iotext','#FFF');
+          root.style.setProperty('--primary-bg','#000');
+          root.style.setProperty('--primary','#FFF');
+          root.style.setProperty('--primary-shadow','rgba(255,255,255,0.5)');
           $(this).find('.darkmode-ico').css('filter','invert(1)');
           isdark = true;
         }else{
-            root.style.setProperty('--iobg','#FFF');
-            root.style.setProperty('--iotext','#000');
+            root.style.setProperty('--primary-bg','#FFF');
+            root.style.setProperty('--primary','#000');
+            root.style.setProperty('--primary-shadow','rgba(0,0,0,0.5)');
             $(this).find('.darkmode-ico').css('filter','invert(0)')
             isdark = false;
         }
--- public/navbar.js
+++ public/navbar.js
@@ -1,9 +1,12 @@
+//everything navbar do
 $( document ).ready(function() {
+    //mobile navbar popup
     let isPoped = false
 
     $(".nav-popup").click(function(){
         $(".mobile-nav").slideToggle("slow",function(){});
     });
 
+    $('')
 
 });
