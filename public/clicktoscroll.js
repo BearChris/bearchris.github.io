12c7e538fe681130c9199f18968efbca2cb541b3
BearChris <bear007509@gmail.com>
2024-06-12 21:37:37 +0800

    added navbar slide

--- public/clicktoscroll.js
+++ public/clicktoscroll.js
@@ -1,30 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     var $backToTop = $(".back-to-top");
     $backToTop.hide();
+    var lastScrollTop = 0
 
-
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         if ($(this).scrollTop() > 100) {
             $backToTop.fadeIn();
         } else {
             $backToTop.fadeOut();
         }
+
+        var st = $(this).scrollTop();
+        if (st > 1) {
+            if (st > lastScrollTop) {
+                // downscroll code
+                $('#navbar').slideUp()
+            } else {
+                // uproll code
+                $('#navbar').slideDown()
+            }
+        } else {
+            $('#navbar').slideDown()
+
+        }
+        lastScrollTop = st
     });
 
-    $backToTop.on('click', function(e) {
-        $("html, body").animate({scrollTop: 0}, 500);
+    $backToTop.on('click', function (e) {
+        $("html, body").animate({ scrollTop: 0 }, 500);
     });
 
-    $(".to-creature").click(function(){
+    $(".to-creature").click(function () {
         $("html, body").animate({
-            scrollTop: $("#second-content").offset().top-100
+            scrollTop: $("#second-content").offset().top - 100
         }, 500);
     });
 
-    $(".to-contact").click(function(){
+    $(".to-contact").click(function () {
         $("html, body").animate({
-            scrollTop: $("#footer").offset().top-100
+            scrollTop: $("#footer").offset().top - 100
         }, 500);
     });
 });
